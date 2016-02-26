fb26b1b64cf116ce090d650db81c5dfc80226676
nic <netzeln@gmail.com>
2016-02-26 11:32:44 -0800

    add functionality for adding clients to stylist

--- app/app.php
+++ app/app.php
@@ -21,20 +21,36 @@
     $app->get('/', function() use ($app)
     {
         $stylists = Stylist::getAll();
-        return $app['twig']->render('index.html.twig', array("stylists"=>$stylists));
+      return $app['twig']->render('index.html.twig', array("stylists"=>$stylists));
     });
 
     $app->post('/add_stylist', function() use ($app)
     {
         $new_stylist = new Stylist($_POST['stylist_name']);
         $new_stylist->save();
-        return $app['twig']->render('index.html.twig', array("stylists"=>Stylist::getAll()));
+      return $app['twig']->render('index.html.twig', array("stylists"=>Stylist::getAll()));
     });
 
     $app->post('/delete_all', function() use ($app)
     {
         Stylist::deleteAll();
-        return $app['twig']->render('index.html.twig', array("stylists"=>Stylist::getAll()));
+      return $app['twig']->render('index.html.twig', array("stylists"=>Stylist::getAll()));
+    });
+
+    $app->get('stylist/{id}', function($id) use ($app)
+    {
+        $stylist = Stylist::find($id);
+        $clients = $stylist->getClients();
+      return $app['twig']->render('stylist.html.twig', array("stylist"=>$stylist, "clients"=> $clients));
+    });
+
+    $app->post('/add_client/{id}', function($id) use ($app)
+    {
+        $stylist_id = $_POST['stylist_id'];
+        $new_client = new Client($_POST['client_name'], $_POST['stylist_id']);
+        $new_client->save();
+        $stylist = Stylist::find($id);
+      return $app['twig']->render('stylist.html.twig', array("clients"=>$stylist->getClients(), "stylist"=> $stylist));
     });
 
 

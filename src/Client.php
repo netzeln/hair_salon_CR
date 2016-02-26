594141c2ae4474ea8dfdec3da9267a55c069396a
nic <netzeln@gmail.com>
2016-02-26 08:29:33 -0800

    add and pass specs for Client get, client constructor

--- src/Client.php
+++ src/Client.php
@@ -1,9 +1,58 @@
 <?php
   class Client
   {
-    private $client_name;
-    private $stylist_id;
-    private $id;
-    
+        private $client_name;
+        private $stylist_id;
+        private $id;
 
+    function __construct($client_name, $stylist_id, $id=null)
+    {
+        $this->client_name = $client_name;
+        $this->stylist_id = $stylist_id;
+        $this->id = $id;
+    }
+
+    //setters
+    function setClientName($new_client_name)
+    {
+        $this->client_name = $new_client_name;
+    }
+    function setStylistId($new_stylist_id)
+    {
+        $this->stylist_id = $new_stylist_id;
+    }
+
+    //getters
+    function getClientName()
+    {
+        return $this->client_name;
+    }
+
+    function getStylistId()
+    {
+        return $this->stylist_id;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    //statics
+
+    function static save()
+    {
+        // $GLOBALS['DB']->exec("INSERT INTO clients (name, stylist_id) VALUES('{$this->getClientName}', {$this->getStylistId});");
+        // $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    function static getAll()
+    {
+
+    }
+
+    function static deleteAll()
+    {
+        
+    }
 } ?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -18,6 +18,50 @@ $DB = new PDO($server, $username, $password);
 
 class  ClientTest  extends PHPUnit_Framework_TestCase{
 
+    function testGetId()
+    {
+        //arrange
+        $name = "Jane Hamilton";
+        $stylist_id = 2;
+        $id = 1;
+        $new_client = new Client ($name, $stylist_id, $id);
+
+        //act
+        $result = $new_client->getId();
+
+        //assert
+        $this->assertEquals(1, $result);
+    }
+
+    function testGetName()
+    {
+        //arrange
+        $name = "Jane Hamilton";
+        $stylist_id = 2;
+        $id = 1;
+        $new_client = new Client ($name, $stylist_id, $id);
+
+        //act
+        $result = $new_client->getClientName();
+
+        //assert
+        $this->assertEquals("Jane Hamilton", $result);
+    }
+
+    function testSave()
+    {
+        //arrange
+        $name = "Jane Hamilton";
+        $stylist_id = 2;
+        $id = 1;
+        $new_client = new Client ($name, $stylist_id, $id);
+
+        //act
+        $new_client->save();
+
+        //assert
+        $
+    }
 
 }
  ?>

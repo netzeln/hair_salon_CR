64505b59cafb016b80ea954dcd659ed41f60ce91
nic <netzeln@gmail.com>
2016-02-26 10:30:38 -0800

    add index.html and / route

--- app/app.php
+++ app/app.php
@@ -1,19 +1,27 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Stylist.php";
 
-    // session_start();
+
 
     $app = new Silex\Application();
 
     $server = 'mysql:host=localhost;dbname=hair_salon';
     $username = 'root';
     $password = 'root';
-    $DB = new PDO($server, $username, $password)
+    $DB = new PDO($server, $username, $password);
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get('/', function() use ($app)
+    {
+        return $app['twig']->render('index.html.twig');
+    });
 
 
     return $app;
